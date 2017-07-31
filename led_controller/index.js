b9d2d2aa851bd11e8adc8553909a2450e990a184
Rens Rooimans <github@rensrooimans.nl>
2017-07-31 17:42:45 +0200

    Removed old fading

--- led_controller/index.js
+++ led_controller/index.js
@@ -11,6 +11,7 @@ var red = 22, // Check
     whi = 25
 
 var _r = 0, _g = 0, _b = 0, _w = 0;
+var _h = 0, _s = 1, _v = 1;
 
  var intensity = 1;
  var fade_speed = 10;
@@ -28,23 +29,16 @@ io.on('connection', function(socket){
   // the separate values
   socket.on('rgbw', function(msg){
     fading = false;
-    Set(msg.r, msg.g, msg.b);
+    Set_RGB(msg.r, msg.g, msg.b);
   });
 
-  // Set the strip to start fading for a given time
-  // or unending when -1 is sent 
-  socket.on('fade', function(msg){
-      piblaster.setPwm(blu, 1); 
-      fading = true;
-      fadeLoop();
-  });
-
+  // Set the strip to start fading with a given intensity
   socket.on('rainbow', function(msg){
       intensity = msg.intensity;
       if(!fading) 
       {
         fading = true;
-        rainbowLoop();
+        hsvloop();
       }
   });
 
@@ -54,91 +48,27 @@ io.on('connection', function(socket){
 });
 
 
-const fadeLoop = async () => {
-  while(fading){
-      // fade from blue to violet
-    for (rc = 0; rc < 101; rc++) { 
-      piblaster.setPwm(red, rc/100); 
-      await snooze(fade_speed);
-    } 
-    // fade from violet to red
-    for (bc = 100; bc > 0; bc--) { 
-      piblaster.setPwm(blu, bc/100);  
-      await snooze(fade_speed);
-    } 
-    // fade from red to yellow
-    for (gc = 0; gc < 101; gc++) { 
-      piblaster.setPwm(gre, gc/100); 
-      await snooze(fade_speed);
-    } 
-    // fade from yellow to gcreen
-    for (rc = 100; rc > 0; rc--) { 
-      piblaster.setPwm(red, rc/100); 
-      await snooze(fade_speed);;
-    } 
-    // fade from gcreen to teal
-    for (bc = 0; bc < 101; bc++) { 
-      piblaster.setPwm(blu, bc/100); 
-      await snooze(fade_speed);
-    } 
-    // fade from teal to blue
-    for (gc = 100; gc > 0; gc--) { 
-      piblaster.setPwm(gre, gc/100);  
-      await snooze(fade_speed);
-    } 
-  }
-}
+const hsvloop = async () => {
+    while(fading)
+    {
+        _h += 0.002;
+        if(_h > 1)
+            _h = 0;
 
-const rainbowLoop = async () => {
-    while(fading){
-        var rc = 0, bc = 100, gc = 0;
-        // fade from blue to violet
-        for (; rc < 101; rc++) {
-            _r = rc; 
-            Set(); 
-            await snooze(fade_speed);
-        } 
-        // fade from violet to red
-        for (; bc > 0; bc--) { 
-            _b = bc;
-            Set();    
-            await snooze(fade_speed);
-        } 
-        // fade from red to yellow
-        for (; gc < 101; gc++) { 
-            _g = gc;
-            Set();  
-            await snooze(fade_speed);
-        } 
-        // fade from yellow to gcreen
-        for (; rc > 0; rc--) { 
-            _r = rc;
-            Set(); 
-            await snooze(fade_speed);
-        } 
-        // fade from gcreen to teal
-        for (; bc < 101; bc++) { 
-            _b = bc;
-            Set();    
-            await snooze(fade_speed);
-        } 
-        // fade from teal to blue
-        for (; gc > 0; gc--) { 
-            _g = gc;
-            Set();  
-            await snooze(fade_speed);
-        } 
+        Set_HSV();
+        await snooze(fade_speed);
     }
 }
+
 
 const fadeOut = async () => {
     fading = false;
     while(intensity > 0)
     {
-        intensity -= 0.005;
+        intensity -= 0.007;
         if(intensity < 0)
             intensity = 0;
-        Set(); 
+        Set_RGB(); 
         await snooze(fade_speed * 5);
     }
 
@@ -147,12 +77,18 @@ const fadeOut = async () => {
 const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 
-function Set(){
+function Set_RGB(){
     console.log(" r:" + _r + " g:"+ _g  + " b:"+ _b  + " I:"+ intensity);
-    piblaster.setPwm(red, _r * intensity / 100); 
-    piblaster.setPwm(gre, _g * intensity / 100);  
-    piblaster.setPwm(blu, _b * intensity / 100);  
-    piblaster.setPwm(whi, _w * intensity / 100);  
+    piblaster.setPwm(red, _r * intensity); 
+    piblaster.setPwm(gre, _g * intensity);  
+    piblaster.setPwm(blu, _b * intensity);  
+    piblaster.setPwm(whi, _w * intensity);  
+}
+
+function Set_HSV(){
+    console.log(" h:" + _h + " s:"+ _s  + " v:"+ _v  + " I:"+ intensity);
+    HSVtoRGB(_h, _s, _v);
+    Set_RGB();
 }
 
 http.listen(3000, function(){
@@ -184,9 +120,7 @@ function HSVtoRGB(h, s, v) {
         case 4: r = t, g = p, b = v; break;
         case 5: r = v, g = p, b = q; break;
     }
-    return {
-        r: r,
-        g: g,
-        b: b
-    };
+    _r = r;
+    _g = g;
+    _b = b;
 }
